2c46f84bd077a2e24c0f2f177a2fea6fc4eb0040
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2023-12-29 13:24:19 -0500

    L

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java -Xms4G -Xmx10G -jar minecraft_server.1.12.2.jar nogui
+java -Xms4G -Xmx10G -jar server.jar nogui
